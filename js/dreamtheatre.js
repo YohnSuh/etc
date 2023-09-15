fab23e445a25745f4e4f1c41905b748babcb2d3f
YohnSuh <yohnsuh@gmail.com>
2023-09-15 19:23:49 +0900

    드림씨어터 연습

---
+++ js/dreamtheatre.js
@@ -0,0 +1,14 @@
+$('.gnbTit').click(function() {
+    if($(window).innerWidth() > 786) {
+        $('.navBg').css({'display' : 'block'});
+        $(this).toggleClass('active');
+        $(this).next().toggleClass('active');
+        $(this).parent().siblings().find('.inner').stop().hide();
+        // $(this).next().addClass('innerMenu');
+        $(this).parent().siblings().find('.gnbTit').removeClass('active');
+        $(this).parent().siblings().find('.inner').removeClass('active');
+    } else {
+        $(this).next().stop().slideToggle();
+        $(this).parent().siblings().find('.inner').stop().slideUp();
+    }
+});
